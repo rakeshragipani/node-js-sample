575f3bd5c218442bf8c2d7ec8d620f6d452dcc0d
rakeshragipani <56871705+rakeshragipani@users.noreply.github.com>
2020-08-13 23:38:06 +0530

    Update lambda.tf

--- lambda-update-tf/lambda.tf
+++ lambda-update-tf/lambda.tf
@@ -8,15 +8,11 @@ data "archive_file" "welcome" {
   output_path = "${local.lambda_zip_location}"
 }
 
-resource "aws_lambda_function" "test_lambda" {
-  filename      = "${local.lambda_zip_location}"
-  function_name = "welcome-nodejs"
-  role          = "${aws_iam_role.iam_for_lambda.arn}"
-  handler       = "index.handler"
- 
-  source_code_hash = "${base64sha256(local.lambda_zip_location)}"
-
-  runtime = "nodejs12.x"
+resource "aws_lambda_alias" "alias" {
+  name             = "${var.name}"
+  description      = "${var.description}"
+  function_name    = "${var.function_name}"
+  function_version = "${var.function_version}"
 }
   
 
