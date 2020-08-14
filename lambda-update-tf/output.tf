40a8b6836c9b77e58d793087bee07c98baaabe3e
rakeshragipani <56871705+rakeshragipani@users.noreply.github.com>
2020-08-14 11:31:14 +0530

    Update output.tf

--- lambda-update-tf/output.tf
+++ lambda-update-tf/output.tf
@@ -1,3 +1,4 @@
-output "lambda_id" {
- value = "${aws_lambda_function.test_lambda.id}"
+output "this_lambda_alias_name" {
+  description = "The name of the Lambda Function Alias"
+  value       = element(concat(data.aws_lambda_alias.existing.*.name, aws_lambda_alias.with_refresh.*.name, aws_lambda_alias.no_refresh.*.name, [""]), 0)
 }
