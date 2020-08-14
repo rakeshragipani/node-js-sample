885e8e835bae0aa1f55d7c0a1a9984209dd0f403
rakeshragipani <56871705+rakeshragipani@users.noreply.github.com>
2020-08-14 12:37:50 +0530

    Update lambda.tf

--- lambda-update-tf/lambda.tf
+++ lambda-update-tf/lambda.tf
@@ -1,5 +1,9 @@
 locals {
   lambda_zip_location = "output/welcome.zip"
+  alias_name = "terraform alias"
+  alias_desc = "create lambda alias"
+  alias_function = "new"
+  alias_func_ver = "tf"
 }
  
 data "archive_file" "welcome" {
@@ -9,10 +13,10 @@ data "archive_file" "welcome" {
 }
 
 resource "aws_lambda_alias" "alias" {
-  name             = "${var.name}"
-  description      = "${var.description}"
-  function_name    = "${var.function_name}"
-  function_version = "${var.function_version}"
+  name             = "${local.alias_name}"
+  description      = "${local.alias_desc}"
+  function_name    = "${local.alias_function}"
+  function_version = "${local.alias_func_ver}"
 }
   
 
