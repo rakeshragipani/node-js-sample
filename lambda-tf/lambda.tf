3d6e960d2baefc0bc15bc1700a0c138ff1c2f9d2
rakeshragipani <56871705+rakeshragipani@users.noreply.github.com>
2020-08-17 17:25:51 +0530

    Update lambda.tf

--- lambda-tf/lambda.tf
+++ lambda-tf/lambda.tf
@@ -10,7 +10,7 @@ data "archive_file" "welcome" {
 
 resource "aws_lambda_function" "test_lambda" {
   filename      = "${local.lambda_zip_location}"
-  function_name = "masterbranch"
+  function_name = "masterbranch1"
   role          = "${aws_iam_role.iam_for_lambda.arn}"
   handler       = "index.handler"
  
